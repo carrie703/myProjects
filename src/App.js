6ead5729e8aa0ec5d90a41e590f33bf308ce197e
carrie703 <n01603699@humber.ca>
2024-09-30 23:23:26 -0400

    footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Game from './components/cards';
+import Footer from './footer';
 
 function App() {
   return (
     <div className="App">
       <h1>Test your memory!</h1>
       <Game></Game>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>Copyright &copy;2024 was developed</p>
+      <p>Author: Carrie</p>
+    </div>
+  );
+}
+
+export default Footer;
